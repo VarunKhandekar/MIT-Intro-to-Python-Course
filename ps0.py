605c9d0dee177ea951e93fc7f52e719442612e7c
VarunKhandekar <80222993+VarunKhandekar@users.noreply.github.com>
2021-03-07 21:26:32 +0000

    Problem Set 0

---
+++ ps0.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  7 12:13:36 2021
+
+@author: Varun
+"""
+
+import math
+import pylab
+
+def exponentiaton_and_logarithm(x, y):
+    print(x**y)
+    print(math.log(x,2))
+    
+    return
+
